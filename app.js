6077a38e81a7a1dae3556bd53b1e91df24610b7b
aina-cas <ainamaplearts@gmail.com.noreply.github.com>
2023-01-16 17:11:07 +0100

    Clean up, refracture and add comments to JS

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
-//Toggle theme function 
-
+// Toggle theme function 
 function changeTheme () {
     document.body.classList.toggle('light');
 }
@@ -8,60 +7,87 @@ function changeTheme () {
 const todoInput = document.querySelector("#todo-input");
 const todos = [];
 
-
-todoInput.addEventListener("keyup",  function (e) {
-    if (e.key === "Enter" || e.keyCode === 13) {  //we check if the property or key is "Enter or keyCode is 13"
-        todos.push({value: e.target.value, checked: false});
-        //console.log(todos);
-        newTodo(e.target.value);
-        todoInput.value = ""; //Clears the input after new input has been pushed to the array 
-
+// Funtction to hadle the user input when they press enter
+function handleTodoInput(event) {
+    // Check if the user pressed the enter key
+    if (event.key === "Enter" || event.keyCode === 13) {
+        // Add the new todo to the todos array
+        todos.push({value: event.target.value, checked: false});
+        //Call the newTodo function to create a new todo item in the html
+        newTodo(event.target.value);
+        //Clear the input field after new todo item is added
+        event.target.value = "";
     }
-})
-
-function newTodo(value) {
-  const todo = document.createElement('div');
-  const todoText = document.createElement('p');
-  const todoCheckBox = document.createElement('input');
-  const todoCheckBoxLabel = document.createElement('label');
-  const todoX = document.createElement('span');
-
-  todoText.textContent = value;
-  todoCheckBox.type = 'checkbox';
-  todoCheckBox.name = 'checkbox';
-  todoCheckBoxLabel.htmlFor = 'checkbox';
-  todoCheckBoxLabel.addEventListener('click', function (e) {
-    if (todoCheckBox.checked) {
-        todoCheckBox.checked = false;
-        todoText.style.textDecoration = 'none';
-        todoCheckBoxLabel.classList.remove('active');
-    } else {
-        todoCheckBox.checked = true;
-        todoText.style.textDecoration = 'line-through';
-        todoCheckBoxLabel.classList.add('active');
-    }
- 
-  });
-
-  todoX.textContent = "X";
-  todoX.addEventListener('click', function(e) {
-    e.target.parentElement.remove();
-  });
-
-  todo.classList.add('todo');
-  todoCheckBoxLabel.classList.add('circle');
-  todoX.classList.add('x');
-
-  todo.appendChild(todoCheckBox);
-  todo.appendChild(todoCheckBoxLabel);
-  todo.appendChild(todoText);
-  todo.appendChild(todoX);
-
-
-  const todosContainer = document.querySelector('.todos-container');
-  todosContainer.appendChild(todo);
-
-
-
-
 }
+
+// Add event listener to the input field to call the handleTodoInput function when enter is pressed 
+todoInput.addEventListener("keyup", handleTodoInput);
+
+// Function to create new todo item element
+function newTodo(value) {
+    // Create the html elements for the todo
+    const todo = document.createElement('div');
+    const todoText = document.createElement('p');
+    const todoCheckBox = document.createElement('input');
+    const todoCheckBoxLabel = document.createElement('label');
+    const todoX = document.createElement('span');
+
+    //Set the text for the element
+    todoText.textContent = value;
+    //Set type and name for checkbox element
+    todoCheckBox.type = 'checkbox';
+    todoCheckBox.name = 'checkbox';
+    //Set the 'for' attribute on the label to match with the checkbox
+    todoCheckBoxLabel.htmlFor = 'checkbox';
+
+    //Add an event listener to the label that will toggle the checkbox and add/remove a class when clicked
+    todoCheckBoxLabel.addEventListener('click', function(e) {
+        if (todoCheckBox.checked) {
+            todoCheckBox.checked = false;
+            todoText.style.textDecoration = 'none';
+            todoCheckBoxLabel.classList.remove('active');
+        } else {
+            todoCheckBox.checked = true;
+            todoText.style.textDecoration = 'line-through';
+            todoCheckBoxLabel.classList.add('active');
+        }
+    });
+
+    todoX.textContent = "X";
+    // Add an event listener to the X elemnt so it will remove the todo item when clicked
+    todoX.addEventListener('click', function(e) {
+        e.target.parentElement.remove();
+    });
+
+    // Add classes to the todo element
+    todo.classList.add('todo');
+    todoCheckBoxLabel.classList.add('circle');
+    todoX.classList.add('x');
+
+    // Append the the elements to the todo item
+    todo.appendChild(todoCheckBox);
+    todo.appendChild(todoCheckBoxLabel);
+    todo.appendChild(todoText);
+    todo.appendChild(todoX);
+
+    // Get the container for todos
+    const todosContainer = document.querySelector('.todos-container');
+
+    // Append the todo to the container
+    todosContainer.appendChild(todo);
+}
+
+
+
+// further functions needed:
+
+// To check if the input is not empty and so the user can not add an empty todo item
+// To store the list after the page is refrehed 
+// To show how many items are left to do on the list 
+// Filter to show all todo items 
+// Filter to show active items 
+// Filter to show already completed items 
+// Clear all the completed 
+// Drag and drop to reorder the list 
+
+//There might be more, but that's all I can think of for now.  
